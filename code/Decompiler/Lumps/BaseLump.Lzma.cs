1c6ade23f1e7db09e9da51538da58fffd7c97b6c
DoctorGurke <docgurk@gmail.com>
2023-02-10 15:22:36 +0100

    trim some unsused + cleanup

--- code/Decompiler/Lumps/BaseLump.Lzma.cs
+++ code/Decompiler/Lumps/BaseLump.Lzma.cs
@@ -12,24 +12,16 @@ public partial class BaseLump
 		var reader = new BinaryReader( new MemoryStream( data ) );
 		var lzmaId = reader.ReadUInt32();
 
-		var compressed = lzmaId == LZMA_ID;
-
-		if ( compressed )
-		{
-			var decompressedData = Decompress( data );
-		}
-
-		return compressed;
+		return lzmaId == LZMA_ID;
 	}
 
 	private byte[] Decompress( byte[] inData )
 	{
 		var sourceHeaderReader = new StructReader<SourceLzmaHeader>();
 		var sourceHeader = sourceHeaderReader.Read( inData );
-		var sourceHeaderLength = 17;
+		var sourceHeaderLength = Marshal.SizeOf<SourceLzmaHeader>();
 
-		var lzmaHeader = new LzmaHeader( sourceHeader );
-		var lzmaHeaderLength = 13;
+		var lzmaHeaderLength = Marshal.SizeOf<LzmaHeader>();
 
 		// lzma body length
 		var bodyLength = inData.Length - sourceHeaderLength;
@@ -65,32 +57,10 @@ public partial class BaseLump
 		decoder.Code( reader.BaseStream, outStream, compressedSize, outSize, null );
 		var outData = outStream.ReadByteArrayFromStream( 0, (uint)outStream.Length );
 
-		if ( outStream.Length == outSize )
-		{
-			//Log.Info( $"decompressed successfully" );
-		}
-
 		return outData;
 	}
 
-	/* lzma decoder properties
-	 * 
-	 * dict size in bytes
-	 * lc - number of high bits of the previous byte to use as a context for literal decoding
-	 * lp - number of low bits of the dict position to include in literal position state
-	 * pb - number of low bits of the dict position to include in position state
-	 */
-
-	/* source lzma header total: 17
-	 * 
-	 * uint id
-	 * uint actualSize
-	 * uint lzmaSize
-	 * char[5] properties
-	 */
-
-
-	[StructLayout( LayoutKind.Sequential )]
+	[StructLayout( LayoutKind.Sequential, Size = 17, Pack = 1 )]
 	private struct SourceLzmaHeader
 	{
 		public uint Id;             // 4
@@ -98,52 +68,13 @@ public partial class BaseLump
 		public uint LzmaSize;       // 4
 		[MarshalAs( UnmanagedType.ByValArray, SizeConst = 5 )]
 		public byte[] Properties;   // 5
-
-		//public override string ToString()
-		//{
-		//	return $"SOURCE LZMA HEADER: actual Size: {ActualSize} lzma Size: {LzmaSize} properties: {BitConverter.ToString( Properties )}\0";
-		//}
 	}
 
-	/* standard lzma header total: 13
-	 * 
-	 * char[5] properties;
-	 * ulong actualSize;
-	 */
-	[StructLayout( LayoutKind.Sequential )]
+	[StructLayout( LayoutKind.Sequential, Size = 13, Pack = 1 )]
 	private struct LzmaHeader
 	{
+		[MarshalAs( UnmanagedType.ByValArray, SizeConst = 5 )]
 		public byte[] Properties;   // 5
 		public ulong ActualSize;    // 8
-
-		public LzmaHeader( SourceLzmaHeader header )
-		{
-			Properties = header.Properties;
-			ActualSize = header.ActualSize;
-		}
-
-		//public override string ToString()
-		//{
-		//	return $"LZMA HEADER: properties: {BitConverter.ToString( Properties )} actual Size: {ActualSize}";
-		//}
 	}
-
-	//private static byte[] LzmaHeaderToByteArray( LzmaHeader header )
-	//{
-	//	int size = 13;//Marshal.SizeOf( header );
-	//	byte[] arr = new byte[size];
-
-	//	IntPtr ptr = IntPtr.Zero;
-	//	try
-	//	{
-	//		ptr = Marshal.AllocHGlobal( size );
-	//		Marshal.StructureToPtr( header, ptr, true );
-	//		Marshal.Copy( ptr, arr, 0, size );
-	//	}
-	//	finally
-	//	{
-	//		Marshal.FreeHGlobal( ptr );
-	//	}
-	//	return arr;
-	//}
 }
