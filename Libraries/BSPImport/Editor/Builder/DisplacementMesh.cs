1e4c014b644baa9528aecf3698dcc1919d8874d2
DoctorGurke <docgurk@gmail.com>
2026-03-27 23:14:17 +0100

    Delete DisplacementMesh.cs

--- Libraries/BSPImport/Editor/Builder/DisplacementMesh.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BspImport.Builder;
-
-internal class DisplacementMesh
-{
-	public DisplacementInfo Info { get; set; }
-
-	public List<DisplacementVertex> Vertices { get; set; } = new();
-}
