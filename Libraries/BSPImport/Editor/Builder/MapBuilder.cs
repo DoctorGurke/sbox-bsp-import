5b4db174d1e3ceea95d5ca5bda91386a8a238709
DoctorGurke <docgurk@gmail.com>
2026-03-31 21:31:36 +0200

    Add progress bar and async a bunch of stuff, major displacement cleanup, temp hack to prevent stall

--- Libraries/BSPImport/Editor/Builder/MapBuilder.cs
+++ Libraries/BSPImport/Editor/Builder/MapBuilder.cs
@@ -1,4 +1,7 @@
-﻿namespace BspImport.Builder;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace BspImport.Builder;
 
 public partial class MapBuilder
 {
@@ -14,21 +17,34 @@ public partial class MapBuilder
 	/// <summary>
 	/// Builds the final decompiled context insto the active editor scene. This will spawn world geometry and map entities, including parsed static props and brush entities.
 	/// </summary>
-	public void Build( GameObject? parent = null )
+	public async void Build( GameObject? parent = null )
 	{
-		using var scope = SceneEditorSession.Scope();
+		using var progress = Editor.Application.Editor.ProgressSection( true );
+		var token = progress.GetCancel();
+
+		// begin scene population
+		using var sceneScope = SceneEditorSession.Scope();
 
 		var name = Context.Name;
-
 		var root = new GameObject( parent, true, name );
 
+		// start timer
+		var stopwatch = new Stopwatch();
+		stopwatch.Start();
 
 		// build map worldspawn geometry (model 0), including displacements
 		if ( Context.Settings.ImportWorldGeometry )
 		{
+			progress.Title = "Building World Geometry";
+			progress.Subtitle = " Test";
+			progress.Icon = "map";
 
-			BuildWorldGeometry( root );
+			await GameTask.Delay( 100 );
+			await BuildWorldGeometry( root, token, progress, meshesPerFrame: 64 );
 		}
+
+		progress.Title = "Building Entities";
+		await GameTask.Delay( 100 );
 
 		// builds entities, including prop static and brush entities
 		if ( Context.Settings.ImportEntities )
@@ -39,7 +55,30 @@ public partial class MapBuilder
 			BuildModelMeshes();
 			BuildEntities( entities );
 		}
+
+		stopwatch.Stop();
+
+		var repoURL = "https://github.com/DoctorGurke/sbox-bsp-import";
+		Log.Info( $"Took {stopwatch.Elapsed.Seconds}s {stopwatch.Elapsed.Milliseconds}ms" );
+		Log.Info( $"Report bugs or contribute @{repoURL}" );
+		Log.Info( $"Imported Source 1 BSP File using sbox-bsp-import by DoctorGurke" );
 	}
 
+	public static async Task<T?> RunWithTimeout<T>(
+	Func<Task<T>> taskFactory,
+	int timeoutMs )
+	{
+		var task = taskFactory();
+		var delay = Task.Delay( timeoutMs );
 
+		var completed = await Task.WhenAny( task, delay );
+
+		if ( completed == delay )
+		{
+			Log.Warning( "Operation timed out" );
+			return default;
+		}
+
+		return await task;
+	}
 }
